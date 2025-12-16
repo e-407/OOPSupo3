e92700585b1a6f0138f8849801b985452976f8af
e-407 <dunne4079@gmail.com>
2025-12-16 18:44:13 +0000

    Q9.3

---
+++ src/main/java/Maths.java
@@ -0,0 +1,15 @@
+public class Maths {
+    public static double root(double k){
+        if (k<0){
+            throw new IllegalArgumentException();
+        }
+        double xprev = 0; //Arbitrary starting values
+        double xn = 1;
+        while (Math.abs(xprev - xn) > 0.0000000000001){
+            xprev = xn;
+            xn = (xn + k/xn)/2;
+        }
+        assert (xn>=0);
+        return xn;
+    }
+}
---
+++ src/test/java/RootTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RootTest {
+
+    @Test
+    void testRoots() {
+        assertEquals(5.0, Maths.root(25.0), 0.00001);
+        assertEquals(1.4142, Maths.root(2.0), 0.0001);
+        assertEquals(0.0, Maths.root(0.0), 0.0000000001);
+        assertThrows(IllegalArgumentException.class, () -> {
+            Maths.root(-5.0);
+        });
+    }
+}
